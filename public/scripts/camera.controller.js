900442e3cb14f918d5ca939d77b8bc1843fae83b
levykohout <levy.kohout@yahoo.com>
2017-01-06 23:15:38 -0600

    b

--- public/scripts/camera.controller.js
+++ public/scripts/camera.controller.js
@@ -1,121 +1,171 @@
 angular.module('myApp')
     .controller('cameraController', function($scope, Upload) {
         var camera = this;
-        var _video = null,
-            patData = null;
-
-        camera.patOpts = {
-            x: 0,
-            y: 0,
-            w: 300,
-            h: 300
-        };
-
-        // Setup a channel to receive a video property
-        // with a reference to the video element
-        // See the HTML binding in camera.html
-        camera.channel = {};
-
-        camera.webcamError = false;
-        camera.onError = function(err) {
-            $scope.$apply(
-                function() {
-                    camera.webcamError = err;
-                }
-            );
-        };
-
-        camera.onSuccess = function() {
-            // The video element contains the captured camera data
-            _video = camera.channel.video;
-            $scope.$apply(function() {
-                camera.patOpts.w = _video.width;
-                camera.patOpts.h = _video.height;
-                //$scope.showDemos = true;
-            });
-        };
-        camera.onStream = function(stream) {
-            // You could do something manually with the stream.
-        };
-
-        camera.makeSnapshot = function() {
-            if (_video) {
-                var patCanvas = document.querySelector('#snapshot');
-                if (!patCanvas) return;
-
-                patCanvas.width = _video.width;
-                patCanvas.height = _video.height;
-                var ctxPat = patCanvas.getContext('2d');
-
-                var idata = getVideoData(camera.patOpts.x, camera.patOpts.y, camera.patOpts.w, camera.patOpts.h);
-                ctxPat.putImageData(idata, 0, 0);
-
-                sendSnapshotToServer(patCanvas.toDataURL());
-
-                patData = idata;
-            }
-        };
-
-        /*** Redirect the browser to the URL given.
-         * Used to download the image by passing a dataURL string
-         */
-        camera.downloadSnapshot = function downloadSnapshot(dataURL) {
-            window.location.href = dataURL;
-        };
-
-        var getVideoData = function getVideoData(x, y, w, h) {
-            var hiddenCanvas = document.createElement('canvas');
-            hiddenCanvas.width = _video.width;
-            hiddenCanvas.height = _video.height;
-            var ctx = hiddenCanvas.getContext('2d');
-            ctx.drawImage(_video, 0, 0, _video.width, _video.height);
-            return ctx.getImageData(x, y, w, h);
-        };
-
-        /**
-         * This function could be used to send the image data
-         * to a backend server that expects base64 encoded images.
-         *
-         * In this example, we simply store it in the scope for display.
-         */
-        var sendSnapshotToServer = function sendSnapshotToServer(imgBase64) {
-            camera.snapshotData = imgBase64;
-            console.log(imgBase64);
-
-        var API_KEY = '25f539a74f88957';
-        camera.data = {};
-
-        // camera.uploadFile = function() {
-            Upload.upload({
-                url: 'https://api.ocr.space/parse/image',
-                data: {
-                    base64Image: camera.snapshotData,
-                    'apikey': API_KEY
-                }
-            }).then(function(resp) {
-                console.log(resp);
-                camera.data = resp.data.ParsedResults[0].ParsedText;
-                console.log(camera.data);
-                console.log('Successful camera image uploaded. Response: ', resp.data);
-                // receipt.processData();
-
-            }, function(resp) {
-                console.log('Error status: ' + resp.status);
-            }, function(evt) {
-                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-            });
-        // }; //End of uploadFile function
-
-    }; //end of sendSnapshotToServer
+    //     var _video = null,
+    //         patData = null;
+    //
+    //     camera.patOpts = {
+    //         x: 0,
+    //         y: 0,
+    //         w: 300,
+    //         h: 300
+    //     };
+    //
+    //     // Setup a channel to receive a video property
+    //     // with a reference to the video element
+    //     // See the HTML binding in camera.html
+    //     camera.channel = {};
+    //
+    //     camera.webcamError = false;
+    //     camera.onError = function(err) {
+    //         $scope.$apply(
+    //             function() {
+    //                 camera.webcamError = err;
+    //             }
+    //         );
+    //     };
+    //
+    //     camera.onSuccess = function() {
+    //         // The video element contains the captured camera data
+    //         _video = camera.channel.video;
+    //         $scope.$apply(function() {
+    //             camera.patOpts.w = _video.width;
+    //             camera.patOpts.h = _video.height;
+    //             //$scope.showDemos = true;
+    //         });
+    //     };
+    //     camera.onStream = function(stream) {
+    //         // You could do something manually with the stream.
+    //     };
+    //
+    //     camera.makeSnapshot = function() {
+    //         if (_video) {
+    //             var patCanvas = document.querySelector('#snapshot');
+    //             if (!patCanvas) return;
+    //
+    //             patCanvas.width = _video.width;
+    //             patCanvas.height = _video.height;
+    //             var ctxPat = patCanvas.getContext('2d');
+    //
+    //             var idata = getVideoData(camera.patOpts.x, camera.patOpts.y, camera.patOpts.w, camera.patOpts.h);
+    //             ctxPat.putImageData(idata, 0, 0);
+    //
+    //             sendSnapshotToServer(patCanvas.toDataURL());
+    //
+    //             patData = idata;
+    //         }
+    //     };
+    //
+    //     /*** Redirect the browser to the URL given.
+    //      * Used to download the image by passing a dataURL string
+    //      */
+    //     camera.downloadSnapshot = function downloadSnapshot(dataURL) {
+    //         window.location.href = dataURL;
+    //     };
+    //
+    //     var getVideoData = function getVideoData(x, y, w, h) {
+    //         var hiddenCanvas = document.createElement('canvas');
+    //         hiddenCanvas.width = _video.width;
+    //         hiddenCanvas.height = _video.height;
+    //         var ctx = hiddenCanvas.getContext('2d');
+    //         ctx.drawImage(_video, 0, 0, _video.width, _video.height);
+    //         return ctx.getImageData(x, y, w, h);
+    //     };
+    //
+    //     /**
+    //      * This function could be used to send the image data
+    //      * to a backend server that expects base64 encoded images.
+    //      *
+    //      * In this example, we simply store it in the scope for display.
+    //      */
+    //     var sendSnapshotToServer = function sendSnapshotToServer(imgBase64) {
+    //         camera.snapshotData = imgBase64;
+    //         console.log(imgBase64);
+    //
+    //     var API_KEY = '25f539a74f88957';
+    //     camera.data = {};
+    //
+    //     // camera.uploadFile = function() {
+    //         Upload.upload({
+    //             url: 'https://api.ocr.space/parse/image',
+    //             data: {
+    //                 base64Image: camera.snapshotData,
+    //                 'apikey': API_KEY
+    //             }
+    //         }).then(function(resp) {
+    //             console.log(resp);
+    //             camera.data = resp.data.ParsedResults[0].ParsedText;
+    //             console.log(camera.data);
+    //             console.log('Successful camera image uploaded. Response: ', resp.data);
+    //             // receipt.processData();
+    //
+    //         }, function(resp) {
+    //             console.log('Error status: ' + resp.status);
+    //         }, function(evt) {
+    //             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+    //             console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+    //         });
+    //     // }; //End of uploadFile function
+    //
+    // }; //end of sendSnapshotToServer
 
 
     var videoElement = document.querySelector('video');
     var audioSelect = document.querySelector('select#audioSource');
     var videoSelect = document.querySelector('select#videoSource');
 
-    navigator.getUserMedia = navigator.getUserMedia ||
-      navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    navigator.getUserMedia  = navigator.getUserMedia ||
+                              navigator.webkitGetUserMedia ||
+                              navigator.mozGetUserMedia ||
+                              navigator.msGetUserMedia;
+
+                          //     if (navigator.getUserMedia) {
+                          //   navigator.getUserMedia({audio: true, video: true}, function(stream) {
+                          //     video.src = window.URL.createObjectURL(stream);
+                          //   }, errorCallback);
+                          // }
+                          //
+                          // MediaStreamTrack.getSources(function(sourceInfos) {
+                          //   var audioSource = null;
+                          //   var videoSource = null;
+                          //
+                          //   for (var i = 0; i != sourceInfos.length; ++i) {
+                          //     var sourceInfo = sourceInfos[i];
+                          //     if (sourceInfo.kind === 'audio') {
+                          //       console.log(sourceInfo.id, sourceInfo.label || 'microphone');
+                          //
+                          //       audioSource = sourceInfo.id;
+                          //     } else if (sourceInfo.kind === 'video') {
+                          //       console.log(sourceInfo.id, sourceInfo.label || 'camera');
+                          //
+                          //       videoSource = sourceInfo.id;
+                          //     } else {
+                          //       console.log('Some other kind of source: ', sourceInfo);
+                          //     }
+                          //   }
+                          //
+                          //   sourceSelected(audioSource, videoSource);
+                          // });
+                          //
+                          // function sourceSelected(audioSource, videoSource) {
+                          //   var constraints = {
+                          //     audio: {
+                          //       optional: [{sourceId: audioSource}]
+                          //     },
+                          //     video: {
+                          //       optional: [{sourceId: videoSource}]
+                          //     }
+                          //   };
+                          //
+                          //   navigator.getUserMedia(constraints, successCallback, errorCallback);
+                          // }
+                          //
+
+
+
+
+
+
 
     function gotSources(sourceInfos) {
       for (var i = 0; i !== sourceInfos.length; ++i) {
@@ -147,7 +197,7 @@ angular.module('myApp')
       videoElement.src = window.URL.createObjectURL(stream);
       videoElement.play();
     }
-
+    //
     function errorCallback(error) {
       console.log('navigator.getUserMedia error: ', error);
     }
@@ -181,6 +231,18 @@ angular.module('myApp')
 
 
 
+    // var _gaq = _gaq || [];
+    // _gaq.push(['_setAccount', 'UA-33848682-1']);
+    // _gaq.push(['_trackPageview']);
+    //
+    // (function() {
+    //   var ga = document.createElement('script');
+    //   ga.type = 'text/javascript';
+    //   ga.async = true;
+    //   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    //   var s = document.getElementsByTagName('script')[0];
+    //   s.parentNode.insertBefore(ga, s);
+    // })();
 
 
 
